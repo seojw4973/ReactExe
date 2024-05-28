d26c8a1ce5337f27e89cacf924ae5525cb71c175
seojw4973 <sgw0816@naver.com>
2024-05-28 18:55:06 +0900

    ch09_lifeCycle 교재 내용 추가

--- ch09_lifecycle/src/App.js
+++ ch09_lifecycle/src/App.js
@@ -1,9 +1,36 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React, { Component } from "react";
 import NotificationList from "./components/NotificationList";
+import LifeCycleSample from "./LifeCycleSample";
 
-function App() {
-  return <NotificationList />;
+function getRamdomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
+
+class App extends Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRamdomColor(),
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color} />
+      </div>
+    );
+  }
+}
+
+// function App() {
+//   // return <NotificationList />;
+// }
 
 export default App;
