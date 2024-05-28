8289fd0cbca0b80f5085b0e1bb19fdc5933e7278
seojw4973 <sgw0816@naver.com>
2024-05-28 16:18:24 +0900

    ch06_event bind하는 이유 주석 추가

--- ch06_event/src/App.js
+++ ch06_event/src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-// import EventPratice from "./ex02_onChange/EventPractice";
+// import EventPractice from "./ex02_onChange/EventPractice";
 // import EventPractice from "./ex03_onChange_state/EventPractice";
 // import EventPractice from "./ex04_onChange_state/EventPractice";
 // import EventPractice from "./ex05_function_bind/EventPractice";
--- ch06_event/src/ex05_function_bind/EventPractice.jsx
+++ ch06_event/src/ex05_function_bind/EventPractice.jsx
@@ -1,45 +1,48 @@
 import { Component } from "react";
 
-class EventPractice extends Component{
+class EventPractice extends Component {
+  state = {
+    message: "",
+  };
 
-    state = {
-        message: ''
-    }
+  constructor(props) {
+    super(props);
 
-    constructor(props){
-        super(props);
+    // Arrow함수가 아닌, 일반 함수는 생성자에서 bind(this)를 처리해야 한다.
+    // 기본적으로 JS 클래스 메서드는 인스턴스에 바인딩되지 않는다.
+    // 이벤트 핸들러 내의 this가 클래스의 인스턴스를 참조하게 하려고 bind를 해주는 것
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-        // Arrow함수가 아닌, 일반 함수는 생성자에서 bind(this)를 처리해야 한다.
-        this.handleChange = this.handleChange.bind(this);
-        this.handleClick = this.handleClick.bind(this);
-    }
+  handleChange(e) {
+    this.setState({
+      message: e.target.value,
+    });
+  }
 
-    handleChange(e){
-        this.setState({
-            message : e.target.value
-        })
-    }
+  handleClick(e) {
+    alert(this.state.message);
+    this.setState({
+      message: "",
+    });
+  }
 
-    handleClick(e){
-        alert(this.state.message);
-        this.setState({
-            message : ''
-        })
-    }
-
-    render() {
-        return (            
-            <div>
-                <h1>이벤트 연습</h1>
-                <input type="text" name="message"
-                    placeholder="아무거나 입력해 보세요"
-                    value={this.state.message}
-                    onChange={this.handleChange}
-                />
-                <button onClick={this.handleClick}>확인</button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>이벤트 연습</h1>
+        <input
+          type="text"
+          name="message"
+          placeholder="아무거나 입력해 보세요"
+          value={this.state.message}
+          onChange={this.handleChange}
+        />
+        <button onClick={this.handleClick}>확인</button>
+      </div>
+    );
+  }
 }
 
 export default EventPractice;
