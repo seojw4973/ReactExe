c32245b3911807133dddd1de71d61d073a5878a1
seojw4973 <sgw0816@naver.com>
2024-05-29 18:53:49 +0900

    SubApp추가

---
+++ ch17_router/src/ex02_sub_app/SubApp.jsx
@@ -0,0 +1,16 @@
+import Home from "./Home";
+import Topics from "./Topics";
+import Contact from "./Contact";
+
+function SubApp() {
+  return (
+    <div>
+      <h1>Hello React Router DOM</h1>
+      <Home />
+      <Topics />
+      <Contact />
+    </div>
+  );
+}
+
+export default SubApp;
