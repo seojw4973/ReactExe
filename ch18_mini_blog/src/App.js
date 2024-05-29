8b4db0b35b5aecd5f0ec3bbd3d7f9ec11603f8fa
seojw4973 <sgw0816@naver.com>
2024-05-29 18:59:14 +0900

    MiniBlogApp 구현하는 중(PostList, PostListItems 구현)

---
+++ ch18_mini_blog/src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import MiniBlogApp from "./ex03_mini_blog_app/MiniBlogApp";
+
+function App() {
+  return <MiniBlogApp />;
+}
+
+export default App;
