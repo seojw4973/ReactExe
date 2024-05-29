f6b9a0a69cae2a6845aa233ec5e698dedfe26609
seojw4973 <sgw0816@naver.com>
2024-05-29 18:53:27 +0900

    ch17 Router-DOM을 위한 컴포넌트 생성

--- ch17_router/src/App.js
+++ ch17_router/src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import SubApp from "./ex09_dynamic_nested/SubApp";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <SubApp />;
 }
 
 export default App;
